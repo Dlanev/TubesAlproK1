a7efd8c735f63b291767ec673c56a64179bcaab4
razwadava <davarazwa@gmail.com>
2025-05-16 17:15:25 +0700

    fondasi beranda

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -2,10 +2,32 @@ import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
   @override
+   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(backgroundColor: Colors.deepPurple),
-      body: Center(child: Text('Beranda')),
+      backgroundColor: Color(0xFFE6E6FA),
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple,
+        title: Text(
+          'Nama Aplikasi',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+          ),
+        ),
+        centerTitle: false,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20), 
+        child: Container(
+          height: 300, 
+          decoration: BoxDecoration(
+            color: Colors.deepPurple[100],
+            borderRadius: BorderRadius.circular(12),
+          ),
+        ),
+      ),
     );
   }
 }
