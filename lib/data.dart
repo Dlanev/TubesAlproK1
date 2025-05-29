75af43a2b9409b6d01a4ed12b1e335c81748da59
razwadava <davarazwa@gmail.com>
2025-05-29 16:35:37 +0700

    Sorting & Searching

--- lib/data.dart
+++ lib/data.dart
@@ -19,3 +19,115 @@ class spend {
 List<spend?> spEnding = List.filled(20, null);
 
 int nData = 0;
+
+void insertionSortSpendingKecilBesar() {
+  for (int i = 1; i < nData; i++) {
+    var key = spEnding[i];
+    int j = i - 1;
+
+    while (j >= 0 && spEnding[j] != null && key != null && (spEnding[j]!.amount ?? 0) > (key.amount ?? 0)) {
+      spEnding[j + 1] = spEnding[j];
+      j = j - 1;
+    }
+    spEnding[j + 1] = key;
+  }
+}
+
+void insertionSortSpendingBesarKecil() {
+  for (int i = 1; i < nData; i++) {
+    var key = spEnding[i];
+    int j = i - 1;
+
+    while (j >= 0 && spEnding[j] != null && key != null && (spEnding[j]!.amount ?? 0) < (key.amount ?? 0)) {
+      spEnding[j + 1] = spEnding[j];
+      j = j - 1;
+    }
+    spEnding[j + 1] = key;
+  }
+}
+
+
+void insertionSortWaktuLamaBaru() {
+  for (int i = 1; i < nData; i++) {
+    var key = spEnding[i];
+    int j = i - 1;
+
+    while (j >= 0 && spEnding[j] != null && key != null) {
+      DateTime keyDate = key.waktu ?? DateTime(1970);
+      DateTime jDate = spEnding[j]!.waktu ?? DateTime(1970);
+
+      if (jDate.isAfter(keyDate)) {
+        spEnding[j + 1] = spEnding[j];
+        j = j - 1;
+      } else {
+        break;
+      }
+    }
+    spEnding[j + 1] = key;
+  }
+}
+
+void insertionSortBaruLama() {
+  for (int i = 1; i < nData; i++) {
+    var key = spEnding[i];
+    int j = i - 1;
+
+    while (j >= 0 && spEnding[j] != null && key != null) {
+      DateTime keyDate = key.waktu ?? DateTime(1970); 
+      DateTime jDate = spEnding[j]!.waktu ?? DateTime(1970);
+
+      if (jDate.isBefore(keyDate)) {
+        spEnding[j + 1] = spEnding[j];
+        j = j - 1;
+      } else {
+        break;
+      }
+    }
+    spEnding[j + 1] = key;
+  }
+}
+
+int binarySearchKategori(String targetTipe) {
+  int left = 0;
+  int right = nData - 1;
+
+  while (left <= right) {
+    int mid = (left + right) ~/ 2;
+    var midSpend = spEnding[mid];
+    String midTipe = midSpend?.tipe ?? '';
+
+    int comparison = midTipe.compareTo(targetTipe);
+    if (comparison == 0) {
+      return mid;
+    } else if (comparison < 0) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return -1;
+}
+
+
+List<int> binarySearchSemuaKategori(String targetTipe) {
+  List<int> hasil = [];
+  int index = binarySearchKategori(targetTipe);
+  
+  if (index == -1) return hasil;
+
+  int left = index;
+  while (left >= 0 && (spEnding[left]?.tipe ?? '') == targetTipe) {
+    left--;
+  }
+
+  int right = index + 1;
+  while (right < nData && (spEnding[right]?.tipe ?? '') == targetTipe) {
+    right++;
+  }
+
+  for (int i = left + 1; i < right; i++) {
+    hasil.add(i);
+  }
+
+  return hasil;
+}
